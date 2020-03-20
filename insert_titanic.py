ba6c9cf4f2fe83d1a2f7187d0eae8e731dde5404
cdixson <crystal.dixson@gmail.com>
2020-03-19 20:31:09 -0700

    updated titanic script

--- insert_titanic.py
+++ insert_titanic.py
@@ -16,11 +16,11 @@ DB_HOST = os.getenv("DB_HOST", default="OOPS")
 ### Connect to ElephantSQL-hosted PostgreSQL
 connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
                         password=DB_PASSWORD, host=DB_HOST)
-print("CONNECTION:", connection)
+#print("CONNECTION:", connection)                                  #check connection
 
 ### A "cursor", a structure to iterate over db records to perform queries
 cursor = connection.cursor()
-print('CURSOR:', cursor)
+#print('CURSOR:', cursor)                                           ###
 
 
 df = pd.read_csv('titanic.csv')
@@ -47,7 +47,7 @@ CREATE TABLE titanic_table (
 
 #data = conn.execute('SELECT * FROM "titanic.sqlite3"').fetchall()
 
-cursor.execute(create_table)
+#cursor.execute(create_table)                                                          #######uncomment later                
 
 #for i in data:
 #  insert_data = """INSERT INTO titanic_table
@@ -72,9 +72,362 @@ cursor.execute(create_table)
     # h/t Jesus and https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.itertuples.html
 
 
-rows = list(df.itertuples(index=False, name=None))
-insertion_query = "INSERT INTO titanic_table (Survived, Pclass, Name, Sex, Age, Sib_Spouses_Count, Parent_Child_Count, Fare) VALUES %s"
-execute_values(cursor, insertion_query, rows)
+#rows = list(df.itertuples(index=False, name=None))
+#insertion_query = "INSERT INTO titanic_table (Survived, Pclass, Name, Sex, Age, Sib_Spouses_Count, Parent_Child_Count, Fare) VALUES %s"   #####uncomment later
+#execute_values(cursor, insertion_query, rows)
+
+#connection.commit()
+
+print('How many passengers survived, and how many died?')
+
+query = """
+SELECT COUNT(titanic_table)
+FROM titanic_table
+WHERE titanic_table.survived > 0
+"""
+cursor.execute(query)
+rows = cursor.fetchone()
+print(rows[0], 'passengers survived')
+
+query2 = """
+SELECT COUNT(titanic_table)
+FROM titanic_table
+WHERE titanic_table.survived < 1
+"""
+cursor.execute(query2)
+rows = cursor.fetchone()
+print(rows[0], 'passengers died')
+print('_______________________________________')
 
 
-connection.commit()
+print('How many passengers were in each class?')
+
+query3 = """
+SELECT COUNT(pclass) FROM titanic_table
+WHERE titanic_table.pclass = 3
+"""
+cursor.execute(query3)
+rows = cursor.fetchone()
+print(rows[0], 'passengers in class 3')
+
+query4 = """
+SELECT COUNT(pclass) FROM titanic_table
+WHERE titanic_table.pclass = 2
+"""
+cursor.execute(query4)
+rows = cursor.fetchone()
+print(rows[0], 'passengers in class 2')
+
+query5 = """
+SELECT COUNT(pclass) FROM titanic_table
+WHERE titanic_table.pclass = 1
+"""
+cursor.execute(query5)
+rows = cursor.fetchone()
+print(rows[0], 'passengers in class 1')
+
+print('_______________________________________')
+
+print('How many passengers survived/died within class 3?')
+
+query6 = """
+SELECT COUNT(pclass) FROM titanic_table
+WHERE titanic_table.pclass = 3 AND survived > 0
+"""
+cursor.execute(query6)
+rows = cursor.fetchone()
+print(rows[0], 'class 3 passengers survived')
+
+query7 = """
+SELECT COUNT(pclass) FROM titanic_table
+WHERE titanic_table.pclass = 3 AND survived < 1
+"""
+cursor.execute(query7)
+rows = cursor.fetchone()
+print(rows[0], 'class 3 passengers died')
+
+print('_______________________________________')
+
+print('How many passengers survived/died within class 2?')
+
+query8 = """
+SELECT COUNT(pclass) FROM titanic_table
+WHERE titanic_table.pclass = 2 AND survived > 0
+"""
+cursor.execute(query8)
+rows = cursor.fetchone()
+print(rows[0], 'class 2 passengers survived')
+
+query9 = """
+SELECT COUNT(pclass) FROM titanic_table
+WHERE titanic_table.pclass = 2 AND survived < 1
+"""
+cursor.execute(query9)
+rows = cursor.fetchone()
+print(rows[0], 'class 2 passengers died')
+
+print('_______________________________________')
+
+print('How many passengers survived/died within class 1?')
+
+query8 = """
+SELECT COUNT(pclass) FROM titanic_table
+WHERE titanic_table.pclass = 1 AND survived > 0
+"""
+cursor.execute(query8)
+rows = cursor.fetchone()
+print(rows[0], 'class 1 passengers survived')
+
+query9 = """
+SELECT COUNT(pclass) FROM titanic_table
+WHERE titanic_table.pclass = 1 AND survived < 1
+"""
+cursor.execute(query9)
+rows = cursor.fetchone()
+print(rows[0], 'class 1 passengers died')
+
+print('_______________________________________')
+
+print('What was the average age of survivors vs nonsurvivors?')
+
+query10 = """
+SELECT AVG(age) FROM titanic_table
+WHERE survived > 0
+"""
+cursor.execute(query10)
+rows = cursor.fetchone()
+print(rows[0], 'is the avearge age of survivors')
+
+query11 = """
+SELECT AVG(age) FROM titanic_table
+WHERE survived < 1
+"""
+cursor.execute(query11)
+rows = cursor.fetchone()
+print(rows[0], 'is the average age of nonsurvivors')
+
+print('_______________________________________')
+
+print('What was the average age for class 3 passengers?')
+
+query12 = """
+SELECT AVG(age) FROM titanic_table
+WHERE pclass = 3
+"""
+cursor.execute(query12)
+rows = cursor.fetchone()
+print(rows[0], 'is the average age for class 3 passengers')
+
+print('_______________________________________')
+
+print('What was the average age for class 2 passengers?')
+
+query13 = """
+SELECT AVG(age) FROM titanic_table
+WHERE pclass = 2
+"""
+cursor.execute(query13)
+rows = cursor.fetchone()
+print(rows[0], 'is the average age for class 2 passengers.')
+
+print('_______________________________________')
+
+print('What was the average age for class 1 passengers?')
+
+query14 = """
+SELECT AVG(age) FROM titanic_table
+WHERE pclass = 1
+"""
+
+cursor.execute(query14)
+rows = cursor.fetchone()
+print(rows[0], 'is the average age for class 1 passengers')
+
+print('_______________________________________')
+
+
+#What was the average fare by passenger class? By survival?
+
+print('What was the average fare for class 3 passengers?')
+
+query15 = """
+SELECT AVG(fare) FROM titanic_table
+WHERE pclass = 3
+"""
+cursor.execute(query15)
+rows = cursor.fetchone()
+print(rows[0], 'was the average fare for class 3 passengers')
+
+print('_______________________________________')
+
+print('What was the average fare for class 2 passengers?')
+
+query16 = """
+SELECT AVG(fare) FROM titanic_table
+WHERE pclass = 2
+"""
+cursor.execute(query16)
+rows = cursor.fetchone()
+print(rows[0], 'was the average fare for class 2 passengers')
+
+print('_______________________________________')
+
+print('What was the average fare for class 1 passengers?')
+
+query17 = """
+SELECT AVG(fare) FROM titanic_table
+WHERE pclass = 1
+"""
+cursor.execute(query17)
+rows = cursor.fetchone()
+print(rows[0], 'was the average fare for class 1 passengers')
+
+print('_______________________________________')
+
+print('What was the average fare for survivors?')
+
+query18 = """
+SELECT AVG(fare) FROM titanic_table
+WHERE survived > 0
+"""
+cursor.execute(query18)
+rows = cursor.fetchone()
+print(rows[0], 'was the average fare for survivors')
+
+print('_______________________________________')
+
+print('What was the average fare for nonsurvivors?')
+
+query19 = """
+SELECT AVG(fare) FROM titanic_table
+WHERE survived < 1
+"""
+cursor.execute(query19)
+rows = cursor.fetchone()
+print(rows[0], 'was the average fare for nonsurvivors')
+
+print('_______________________________________')
+
+print('What was the average number of siblings/spouses aboard for class 3?')
+
+query20 = """
+SELECT AVG(sib_spouses_count) FROM titanic_table
+WHERE pclass = 3
+"""
+cursor.execute(query20)
+rows = cursor.fetchone()
+print(rows[0], 'was the average number siblings/spouses aboard in class 3')
+print('_______________________________________')
+
+print('What was the average number of parents/children aboard for class 2?')
+
+query21 = """
+SELECT AVG(sib_spouses_count) FROM titanic_table
+WHERE pclass = 2
+"""
+cursor.execute(query21)
+rows = cursor.fetchone()
+print(rows[0], 'was the average number siblings/spouses aboard in class 2')
+print('_______________________________________')
+
+print('What was the average number of parents/children aboard for class 1?')
+
+query22 = """
+SELECT AVG(sib_spouses_count) FROM titanic_table
+WHERE pclass = 1
+"""
+cursor.execute(query22)
+rows = cursor.fetchone()
+print(rows[0], 'was the average number siblings/spouses aboard in class 1')
+print('_______________________________________')
+
+print('What was the average number of siblings/spouses aboard who survived?')
+
+query23 = """
+SELECT AVG(sib_spouses_count) FROM titanic_table
+WHERE survived > 0
+"""
+cursor.execute(query23)
+rows = cursor.fetchone()
+print(rows[0], 'was the average number siblings/spouses aboard who survived')
+print('_______________________________________')
+
+print('What was the average number of siblings/spouses aboard who did not survive?')
+
+query24 = """
+SELECT AVG(sib_spouses_count) FROM titanic_table
+WHERE survived < 1
+"""
+cursor.execute(query24)
+rows = cursor.fetchone()
+print(rows[0], 'was the average number parents/children aboard who did not survive')
+print('_______________________________________')
+
+print('What was the average number of parents/children aboard for class 3?')                   
+
+query25 = """
+SELECT AVG(parent_child_count) FROM titanic_table
+WHERE pclass = 3
+"""
+cursor.execute(query25)
+rows = cursor.fetchone()
+print(rows[0], 'was the average number parents/children aboard in class 3')
+print('_______________________________________')
+
+print('What was the average number of parents/children aboard for class 2?')
+
+query26 = """
+SELECT AVG(parent_child_count) FROM titanic_table
+WHERE pclass = 2
+"""
+cursor.execute(query26)
+rows = cursor.fetchone()
+print(rows[0], 'was the average number parents/children aboard in class 2')
+print('_______________________________________')
+
+print('What was the average number of parents/children aboard for class 1?')
+
+query27 = """
+SELECT AVG(parent_child_count) FROM titanic_table
+WHERE pclass = 1
+"""
+cursor.execute(query27)
+rows = cursor.fetchone()
+print(rows[0], 'was the average number parents/children aboard in class 1')
+print('_______________________________________')
+
+print('What was the average number of parents/children aboard who survived?')
+
+query28 = """
+SELECT AVG(parent_child_count) FROM titanic_table
+WHERE survived > 0
+"""
+cursor.execute(query28)
+rows = cursor.fetchone()
+print(rows[0], 'was the average number parents/children aboard who survived')
+print('_______________________________________')
+
+print('What was the average number of parents/children aboard who did not survive?')
+
+query29 = """
+SELECT AVG(parent_child_count) FROM titanic_table
+WHERE survived < 1
+"""
+cursor.execute(query29)
+rows = cursor.fetchone()
+print(rows[0], 'was the average number parents/children aboard who did not survive')
+print('_______________________________________')
+
+print("How many passengers have the same name?")
+
+query31 = """
+SELECT name FROM titanic_table
+GROUP BY name 
+HAVING(COUNT(name) > 1)
+"""
+
+cursor.execute(query31)
+rows = cursor.fetchone()
+print(rows)
+
+
